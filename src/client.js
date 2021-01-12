151b687aa07f835bcab3fbb66d520891ed63fcff
speak-free <77342345+speak-free@users.noreply.github.com>
2021-01-13 01:09:28 +0200

    Update client.js

--- src/client.js
+++ src/client.js
@@ -2,6 +2,6 @@
 
 import ReactDOM from 'react-dom';
 import React from 'react';
-import App from './src/App.js';
+import App from './App.js';
 
 ReactDOM.render(<App />, document.getElementById('root'))
